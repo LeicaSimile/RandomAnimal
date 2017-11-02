75a360100dfe2ae86f8f8afe7b5282675a63a81d
LeicaSimile <leicasimile@gmail.com>
2017-11-01 23:52:11 -0400

    Add basic project structure

---
+++ app/src/main/java/leicasimile/randomanimal/WordGeneratorActivity.kt
@@ -0,0 +1,12 @@
+package leicasimile.randomanimal
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class WordGeneratorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_word_generator)
+    }
+}
