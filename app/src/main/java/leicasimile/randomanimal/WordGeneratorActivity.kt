4d9cee862d9434fdb66a7da706caadcc6c99f3f8
LeicaSimile <leicasimile@gmail.com>
2017-11-09 22:35:35 -0500

    Add empty handler for btnFetch click

--- app/src/main/java/leicasimile/randomanimal/WordGeneratorActivity.kt
+++ app/src/main/java/leicasimile/randomanimal/WordGeneratorActivity.kt
@@ -2,11 +2,16 @@ package leicasimile.randomanimal
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_word_generator.*
 
 class WordGeneratorActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_generator)
+    }
+
+    fun btnFetchClicked(view: View) {
+
     }
 }
