a8d8b4815b39a9c74561a8750ab4d2a969b4d101
LeicaSimile <leicasimile@gmail.com>
2017-11-10 20:28:10 -0500

    Add objects to represent animals and adjectives tables in db

---
+++ app/src/main/java/leicasimile/randomanimal/Table.kt
@@ -0,0 +1,13 @@
+package leicasimile.randomanimal
+
+object AnimalsTable {
+    val TABLE_NAME = "animals"
+    val ID = "id"
+    val NAME = "name"
+}
+
+object AdjectivesTable {
+    val TABLE_NAME = "adjectives"
+    val ID = "id"
+    val NAME = "name"
+}
